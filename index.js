afa80e77d9e22ca21ecc25fc9c1682417abd9406
“Keren <kerencharles4@gmail.com>
2022-01-27 12:07:40 -0500

    connected to firestore and added restaurant

--- index.js
+++ index.js
@@ -1,10 +1,36 @@
 // import our restaurants
 const restaurants = require("./restaurants.json")
 
-console.log(restaurants[0].name)
+// import a set of tools to talk to firebase and Firestore
+const {
+  initializeApp,
+  applicationDefault,
+  cert,
+} = require("firebase-admin/app")
+const {
+  getFirestore,
+  Timestamp,
+  FieldValue,
+} = require("firebase-admin/firestore")
+
+// import our credentials
+const credentials = require("./credentials.json")
+
+// connect to firebase services
+initializeApp({
+  credential: cert(credentials),
+})
 
 // connect to Firestore
+const db = getFirestore()
 
-// create a collection called "restaurants"
+// create a collections called "restaurants"
+db.collection("restaurants")
+  .add(restaurants[1])
+  .then((doc) => {
+    console.log("Added restaurant", doc.id)
+  })
 
-// add each restaurant
+  .catch((err) => {
+    console.error(err)
+  })
